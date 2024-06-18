82c5efa2ddb86dddb1f497dd8db3269209702b78
pedroafonso-proof <pedro.afonso@proof.com.br>
2024-06-17 21:12:07 -0300

    fix: update treesitter

--- nvim/lua/plugins/plugin_manifest/05-treesitter.lua
+++ nvim/lua/plugins/plugin_manifest/05-treesitter.lua
@@ -1,14 +1,35 @@
 local M = {}
 
 table.insert(M, {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      lazy = true,
-    },
-  },
-  build = ":TSUpdate",
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
+		treesitter.setup({
+			highlight = {
+				enable = true,
+			},
+			indent = { enable = true },
+			autotag = {
+				enable = true,
+			},
+			ensure_installed = {
+				"lua",
+				"python",
+				"typescript",
+				"html",
+				"css",
+				"svelte",
+				"dockerfile",
+				"markdown",
+				"markdown_inline",
+			},
+		})
+	end,
 })
 
 return M
