0a077d380277a26a1ec332ab27bea44c8f131ecc
pedroafonso-proof <pedro.afonso@proof.com.br>
2024-06-17 21:10:03 -0300

    feat: add lazygit

---
+++ nvim/lua/plugins/plugin_manifest/05-lazygit.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+table.insert(M, {
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+})
+
+return M
